1896796301e87bc21a485f6393578afaac79f000
makiart <haruki3968@gmail.com>
2023-11-19 00:27:13 +0900

    Add: random positon vortex back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,42 @@
 "use client";
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { MdTranslate } from "react-icons/md";
 import { MdOutlineAssignment } from "react-icons/md";
+import { Parallax } from "react-scroll-parallax";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 export default function Home() {
+  const linkDivRef = useRef<HTMLDivElement>(null);
   const [vortexPositions, setVortexPositions] = useState([
-    { x: 0, y: 0, size: 0, zIndex: 0 },
-    { x: 0, y: 0, size: 0, zIndex: 0 },
-    { x: 0, y: 0, size: 0, zIndex: 0 },
-    { x: 0, y: 0, size: 0, zIndex: 0 },
-    { x: 0, y: 0, size: 0, zIndex: 0 },
-    { x: 0, y: 0, size: 0, zIndex: 0 },
-    { x: 0, y: 0, size: 0, zIndex: 0 },
-    { x: 0, y: 0, size: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
+    { x: 0, y: 0, size: 0, speed: 0, zIndex: 0 },
   ]);
-
-  const getWindowSize = () => {
-    return {
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  };
 
   // vortexをランダムに配置する関数
   const randomizeVortexPositions = () => {
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+    const windowWidth = linkDivRef.current?.scrollWidth || 0;
+    const windowHeight = linkDivRef.current?.scrollHeight || 0;
     const windowSize = windowHeight * windowWidth;
-
-    // const newVortexPositions = Array.from({ length: vortexCount }, () => ({
-    //   x: Math.random() * (windowSize.width - maxSize),
-    //   y: Math.random() * (windowSize.height - maxSize),
-    //   size: Math.random() * (maxSize - minSize) + minSize,
-    //   zIndex:
-    //     Math.floor(Math.random() * (maxZIndex - minZIndex + 1)) + minZIndex,
-    // }));
+    console.log(windowHeight, windowWidth, windowSize);
 
     const newVortexPositions = vortexPositions.map(() => {
       const size =
-        Math.random() * Math.max(windowWidth, windowHeight) * 0.8 +
-        Math.min(windowWidth, windowHeight) * 0.1;
+        Math.min(windowHeight, windowWidth) * Math.random() * 0.7 +
+        Math.min(windowHeight, windowWidth) * 0.4;
       return {
         x: Math.random() * windowWidth - size / 2,
         y: Math.random() * windowHeight - size / 2,
         size: size,
+        // sizeが大きいほど値が０に近づく
+        speed: -(1 - size / windowSize) * 10,
         zIndex: 0,
       };
     });
@@ -64,94 +55,101 @@ export default function Home() {
   }, []);
 
   return (
-    <main>
-      <div className="z-0">
-        {/* 渦画像たち */}
-        {vortexPositions.map((vortex, index) => (
-          <img
-            key={index}
-            src={`/vortex/0${index + 1}_transparent.png`}
-            alt={`渦${index + 1}`}
-            className="fixed pointer-events-none top-0 left-0 w-1/2"
-            style={{
-              transform: `translate(${vortex.x}px, ${vortex.y}px)`,
-              width: `${vortex.size}px`,
-              zIndex: vortex.zIndex,
-            }}
-          />
-        ))}
-      </div>
-      <div className="min-h-screen overflow-scroll p-3 background-blur bg-white bg-opacity-20">
-        <div className="container mx-auto py-12">
-          <div className="flex flex-col gap-3 justify-center items-center">
+    <ParallaxProvider>
+      <main className="overflow-hidden">
+        <div
+          className="min-h-screen overflow-y-scroll overflow-x-hidden relative"
+          ref={linkDivRef}
+        >
+          {/* 渦画像たち */}
+          {vortexPositions.map((vortex, index) => (
+            // <Parallax key={index} speed={vortex.speed}>
             <img
-              src="tedxutsukuba.png"
-              alt="TEDxUTsukuba"
-              className="w-[60%] mx-auto max-w-md"
+              key={index}
+              src={`/vortex/0${index + 1}_transparent.png`}
+              alt={`渦${index + 1}`}
+              className="absolute pointer-events-none"
+              style={{
+                transform: `translate(${vortex.x}px, ${vortex.y}px)`,
+                width: `${vortex.size}px`,
+                zIndex: vortex.zIndex,
+              }}
             />
-            <h1 className="text-5xl font-bold text-center font-serif">
-              万華鏡
-            </h1>
-          </div>
-          <div className="py-12">
-            <div className="flex justify-start">
-              <div className="relative max-w-[200px]">
+            // </Parallax>
+          ))}
+          <div className="background-blur bg-white p-3 bg-opacity-20">
+            <div className="container mx-auto py-12">
+              <div className="flex flex-col gap-3 justify-center items-center">
                 <img
-                  src="/vortex/01_transparent.png"
-                  alt="渦1"
-                  className="w-full active:rotate-30 transition-all duration-500"
+                  src="tedxutsukuba.png"
+                  alt="TEDxUTsukuba"
+                  className="w-[60%] mx-auto max-w-md"
                 />
-                <div className="absolute rounded-full w-[70%] h-[70%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-purple-100 border-2 bg-opacity-50 backdrop-blur-sm border-purple-400 flex justify-center items-center">
-                  <div className="flex flex-col gap-2 items-center">
-                    <MdTranslate className="text-5xl text-purple-800" />
-                    <p className="text-xl font-bold text-center text-purple-800 font-serif">
-                      翻訳
-                    </p>
-                  </div>
-                </div>
+                <h1 className="text-5xl font-bold text-center font-serif">
+                  万華鏡
+                </h1>
               </div>
-            </div>
-            <div className="flex justify-end">
-              <div className="relative max-w-[200px]">
-                <img
-                  src="/vortex/03_transparent.png"
-                  alt="渦1"
-                  className="w-full active:rotate-30 transition-all duration-500"
-                />
-                <div className="absolute rounded-full w-[70%] h-[70%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-cyan-100 border-2 bg-opacity-50 backdrop-blur-sm border-cyan-400 flex justify-center items-center">
-                  <div className="flex flex-col gap-2">
+              <div className="py-12">
+                <div className="flex justify-start">
+                  <div className="relative w-[70%]">
                     <img
-                      src="CSLogo.svg"
-                      alt="Comment Screen"
-                      className="w-10 mx-auto"
+                      src="/vortex/01_transparent.png"
+                      alt="渦1"
+                      className="w-full active:rotate-30 transition-all duration-500"
                     />
-                    <p className="text-xs font-bold text-center text-cyan-800 font-serif">
-                      Comment Screen
-                    </p>
+                    <div className="absolute rounded-full w-[70%] h-[70%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-purple-100 border-2 bg-opacity-50 backdrop-blur-sm border-purple-400 flex justify-center items-center">
+                      <div className="flex flex-col gap-2 items-center">
+                        <MdTranslate className="text-5xl text-purple-800" />
+                        <p className="text-xl font-bold text-center text-purple-800 font-serif">
+                          翻訳
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="flex justify-start">
-              <div className="relative max-w-[200px]">
-                <img
-                  src="/vortex/07_transparent.png"
-                  alt="渦1"
-                  className="w-full active:rotate-30 transition-all duration-500"
-                />
-                <div className="absolute rounded-full w-[70%] h-[70%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-pink-100 border-2 bg-opacity-50 backdrop-blur-sm border-pink-400 flex justify-center items-center">
-                  <div className="flex flex-col gap-2 items-center">
-                    <MdOutlineAssignment className="text-5xl text-pink-800" />
-                    <p className="text-xl font-bold text-center text-pink-800 font-serif">
-                      アンケート
-                    </p>
+                <div className="flex justify-end">
+                  <div className="relative w-[70%]">
+                    <img
+                      src="/vortex/03_transparent.png"
+                      alt="渦1"
+                      className="w-full active:rotate-30 transition-all duration-500"
+                    />
+                    <div className="absolute rounded-full w-[70%] h-[70%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-cyan-100 border-2 bg-opacity-50 backdrop-blur-sm border-cyan-400 flex justify-center items-center">
+                      <div className="flex flex-col gap-2">
+                        <img
+                          src="CSLogo.svg"
+                          alt="Comment Screen"
+                          className="w-10 mx-auto"
+                        />
+                        <p className="text-xs font-bold text-center text-cyan-800 font-serif">
+                          Comment Screen
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex justify-start">
+                  <div className="relative w-[70%]">
+                    <img
+                      src="/vortex/07_transparent.png"
+                      alt="渦1"
+                      className="w-full active:rotate-30 transition-all duration-500"
+                    />
+                    <div className="absolute rounded-full w-[70%] h-[70%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-pink-100 border-2 bg-opacity-50 backdrop-blur-sm border-pink-400 flex justify-center items-center">
+                      <div className="flex flex-col gap-2 items-center">
+                        <MdOutlineAssignment className="text-5xl text-pink-800" />
+                        <p className="text-xl font-bold text-center text-pink-800 font-serif">
+                          アンケート
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </ParallaxProvider>
   );
 }
